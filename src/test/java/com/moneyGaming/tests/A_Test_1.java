4de69ea99f642a4e323541167b9e4e47ad06dfb8
imran1066 <85904544+imran1066@users.noreply.github.com>
2021-09-22 03:15:41 +0100

    My first commit to my Branch

--- src/test/java/com/moneyGaming/tests/A_Test_1.java
+++ src/test/java/com/moneyGaming/tests/A_Test_1.java
@@ -1,4 +1,25 @@
 package com.moneyGaming.tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class A_Test_1 {
+    @BeforeMethod
+    public void setUp(){
+
+    }
+
+    @Test
+    public void test1(){
+
+    }
+    @AfterMethod
+    public void tearDown(){
+
+    }
+
+
+}
 }
