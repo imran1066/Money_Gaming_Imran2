423975b38eefc5f1bf436ea2d6d8345d60972fd9
imran1066 <85904544+imran1066@users.noreply.github.com>
2021-09-24 19:14:40 +0100

    This is the testing GitHub.

--- src/test/java/com/moneyGaming/tests/A_Test_1.java
+++ src/test/java/com/moneyGaming/tests/A_Test_1.java
@@ -1,5 +1,6 @@
 package com.moneyGaming.tests;
 
+import com.moneyGaming.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -9,6 +10,10 @@ import org.testng.annotations.Test;
 public class A_Test_1 {
     @BeforeMethod
     public void setUp(){
+//opens me a chrome driver
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
 
 
     }
