4701486b54463d6b6633f6d1076d815ef44645fc
imran1066 <85904544+imran1066@users.noreply.github.com>
2021-09-27 03:45:48 +0100

    I have second time on 250921 added  my WDFactory utility class. Now machine is not working.

--- src/test/java/com/moneyGaming/tests/A_Test_1.java
+++ src/test/java/com/moneyGaming/tests/A_Test_1.java
@@ -5,40 +5,39 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class A_Test_1 {
+class A_Test_1 {
 
     WebDriver driver;
 
-    @BeforeMethod
-    public void setUp(){
+        @BeforeMethod
+        public void setUp(){
 //opens me a chrome driver
-        WebDriver driver= WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.get("https://moneygaming.qa.gameaccount.com/sign-up.shtml");
+            WebDriver driver= WebDriverFactory.getDriver("chrome");
+            driver.manage().window().maximize();
+            driver.get("https://moneygaming.qa.gameaccount.com/sign-up.shtml");
 
-    }
+        }
 
-    @Test
-    public void test1() throws InterruptedException {
-        WebElement nameBox= driver.findElement(By.cssSelector("#forename"));
-        nameBox.sendKeys("Adam");
+        @Test
+        public void test1() throws InterruptedException {
+            WebElement nameBox= driver.findElement(By.cssSelector("#forename"));
+            nameBox.sendKeys("Adam");
 
-        WebElement surnameBox = driver.findElement(By.xpath("//input[@name='map(lastName)']"));
+            WebElement surnameBox = driver.findElement(By.xpath("//input[@name='map(lastName)']"));
 
-        surnameBox.sendKeys("Smith");
+            surnameBox.sendKeys("Smith");
 
-        Thread.sleep(3000);
+            Thread.sleep(3000);
 
 
 
-    }
-    @AfterMethod
-    public void tearDown(){
-        driver.close();
+        }
+        @AfterMethod
+        public void tearDown(){
+            driver.close();
 
     }
 
